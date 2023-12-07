b9d7c9283fd1682571d3d9197ad04edf9a23fbc4
ignacio-herrera <iehp2013@gmail.com>
2023-12-07 19:34:05 -0300

    tequenos-beta3

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 const contenidoTienda = document.getElementById("contenidoTienda");
+const contenidoClientes = document.getElementById("contenidoClientes");
 const activarCarrito = document.getElementById("activarCarrito");
 const carritoContenedor = document.getElementById("carritoContenedor");
 const contadorCarrito = document.getElementById("contadorCarrito");
 const toastBtn = document.querySelector("#toastify")
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+
+clientes.forEach((clientes)=> {
+    let clientesCard = document.createElement("div");
+    clientesCard.className = "cardClientes"
+    clientesCard.innerHTML = `
+    <img class="clientes-img" src="${clientes.img}">
+    <p class="titulo-clientes">${clientes.nombre}</p>
+    <p class="clientes-descripcion">${clientes.descripcion}<p>
+    <img class="rating" src="${clientes.stars}">
+    `;
+
+    contenidoClientes.append(clientesCard)});
 
 // Generador de productos en tienda
     productos.forEach((album)=> {
---
+++ js/clientes.js
@@ -0,0 +1,38 @@
+const clientes= [
+    {   
+        img: "./assets/img/veneventas.jpeg",
+        nombre: "Veneventas Market",
+        descripcion: "Excelencia en los productos, muy buena presentación y buen margen de ganancia. Como revendedor de sus productos, muy buena experiencia. Seguiremos vendiendo.",
+        stars: "./assets/img/rating.png",
+    },
+    {   
+        img: "./assets/img/inatti.jpeg",
+        nombre: "Inatti Burger",
+        descripcion: "De verdad que son los mejores tequeños, todos nuestros clientes lo mencionan y es uno de los productos más vendidos, personalmente somos fan de sus productos.",
+        stars: "./assets/img/rating.png",
+    },
+    {   
+        img: "./assets/img/veneventas.jpeg",
+        nombre: "Maracaibo Mia",
+        descripcion: "Tenemos 3 años trabajando de la mano de Tequedelicias y sus tequeños son sin lugar a duda los mejores de Concepción. Cada cliente que los prueba vuelve a comprar.",
+        stars: "./assets/img/rating.png",
+    },
+    {   
+        img: "./assets/img/smile.jpeg",
+        nombre: "Smile Comida Rápida",
+        descripcion: "Producto 100% confiable, con una exquisita y suave masa, acompañada del mejor queso que le da ese sabor inconfundible. Cpmo cliente lo recomiendo, una grata experiencia de sabor garantizada.",
+        stars: "./assets/img/rating.png",
+    },
+    {   
+        img: "./assets/img/nawuara.jpeg",
+        nombre: "Nawuara",
+        descripcion: "Le agradecemos a @tequedelicia.cl por su excelente productos y de calidad, además de su respuesta rápida a los pedidos. Más su cordialidad para atender.. Y destacar el sabor de nuestro país que nos identifica..",
+        stars: "./assets/img/rating.png",
+    },
+    {   
+        img: "./assets/img/chamos.jpeg",
+        nombre: "Shawarmas Chamos",
+        descripcion: "¡Excelentes productos y de la mejor calidad! Agradecidos sinceramente por el servicio que ofrecen y su amable atención, disponibilidad, responsabilidad y puntualidad. Recomendados 100%",
+        stars: "./assets/img/rating.png",
+    },
+];
