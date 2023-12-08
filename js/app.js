8428de73ad4f065dc853e7c5807ccfafea0beca2
ignacio-herrera <iehp2013@gmail.com>
2023-12-08 11:52:49 -0300

    beta7

--- js/app.js
+++ js/app.js
@@ -8,6 +8,26 @@ const toastBtn = document.querySelector("#toastify")
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 
+// SCROLL A SECCIONES
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav ul li a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
 // Generador de productos en tienda
     productos.forEach((album)=> {
         let contenido = document.createElement("div");
@@ -56,22 +76,3 @@ localStorage.setItem("carrito", JSON.stringify(carrito));
 };
 carritoContador();
 
-
-// SCROLL A SECCIONES
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav ul li a');
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-};
